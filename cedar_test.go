1906d60e66383000afc2ea0750df3c80869afae1
Anton Södergren <anton@zook.ai>
2019-05-27 23:03:44 +0200

    failing test: cedar.Save() fails for both gob and json

--- cedar_test.go
+++ cedar_test.go
@@ -2,6 +2,7 @@ package cedar
 
 import (
 	"fmt"
+	"os"
 	"testing"
 )
 
@@ -40,4 +41,62 @@ func TestInsertFloat(t *testing.T) {
 		fmt.Printf("key:%s val:%f\n", string(k), v.(float32))
 	}
 	cd.DumpGraph("datrie.gv")
+}
+
+func TestCedar_Save(t *testing.T) {
+	cd := createCedar(t)
+	formats := []string{"json", "gob"}
+	for _, format := range formats {
+		testCedarFormat(t, cd, format)
+	}
+
+}
+
+func createCedar(t *testing.T) *Cedar {
+	t.Helper()
+	cd := NewCedar()
+	words := []string{
+		"she", "hers", "her", "he",
+	}
+	for i, word := range words {
+		cd.Insert([]byte(word), i)
+	}
+	return cd
+}
+
+func testCedarFormat(t *testing.T, cd *Cedar, format string) {
+	t.Helper()
+	fileName := "test.tmp"
+	f, err := os.Create(fileName)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer os.Remove(fileName)
+	err = cd.Save(f, format)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	cd2 := NewCedar()
+	err = cd2.LoadFromFile(fileName, format)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	keys, nodes, size, cap := cd.Status()
+	keys2, nodes2, size2, cap2 := cd2.Status()
+	if keys != keys2 {
+		t.Errorf("Had %v keys, but reloaded %s has %v", keys, format, keys2)
+	}
+	if nodes != nodes2 {
+		t.Errorf("Had %v nodes, but reloaded %s has %v", nodes, format, nodes2)
+	}
+	if size != size2 {
+		t.Errorf("Had %v size, but reloaded %s has %v", size, format, size2)
+	}
+	if cap != cap2 {
+		t.Errorf("Had %v cap, but reloaded %s has %v", cap, format, cap2)
+	}
 }
